d45a3d597971663547bab451809363beae26e13d
Tusar78 <tusarahmed141@gmail.com>
2025-12-29 11:13:42 +0600

    Small tax discount problem solved

--- Day 01/app.js
+++ Day 01/app.js
@@ -1,15 +1,24 @@
+// let subtotal = 1000;
+// let tax = subtotal * 0.05;
+// let total = subtotal + tax;
+// console.log(tax, total);
+
+
+// const deliveryCharge = 40;
+// let cartTotal = 1000;
+// let isPremiumUser = true;
+
+// if (isPremiumUser) {
+//     cartTotal -= deliveryCharge;
+// }
+
+// console.log(cartTotal);
+
+const discount = 0.1;
+const tax = 0.05;
 let subtotal = 1000;
-let tax = subtotal * 0.05;
-let total = subtotal + tax;
-console.log(tax, total);
+subtotal -= subtotal * discount;
+subtotal += subtotal * tax;
+console.log("Final Price: " + subtotal);
 
 
-const deliveryCharge = 40;
-let cartTotal = 1000;
-let isPremiumUser = true;
-
-if (isPremiumUser) {
-    cartTotal -= deliveryCharge;
-}
-
-console.log(cartTotal);
