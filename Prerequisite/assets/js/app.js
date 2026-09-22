a0772f5bc51b1912dde6610a979c7293c4af87b7
Tusar78 <tusarahmed141@gmail.com>
2026-09-22 06:03:21 +0600

    Again revise about rest and spreed

--- Prerequisite/assets/js/app.js
+++ Prerequisite/assets/js/app.js
@@ -60,35 +60,136 @@
 // console.log(name3);
 
 
-// Task 01
-const developer = {
-    name: "Tusar",
-    experience: 4,
-    skill: "Webflow"
-}
+// // Task 01
+// const developer = {
+//     name: "Tusar",
+//     experience: 4,
+//     skill: "Webflow"
+// }
 
-const {name, experience, skill} = developer;
+// const {name, experience, skill} = developer;
+
+// // Task 02
+// const user = {
+//     name: "Tusar",
+//     age: 28
+// };
+// const {name: userName, age: userAge} = user;
+
+// // Task 03
+// const skills = ["HTML", "CSS", "JavaScript", "React"];
+// const [firstSkill, secondSkill, thirdSkill, fourthSkill] = skills;
+
+// // Task 04
+// const user = {
+//     name: "Tusar",
+//     role: "Developer",
+//     country: "Bangladesh"
+// };
+
+// const showUser = user => {
+//     const {name, role, country} = user;
+//     return `${name} is a ${role} from ${country}`
+// }
+// showUser(user);
+
+
+// const numbers = [10, 20, 30];
+// console.log(numbers);
+
+// const newNumber = [0, ...numbers, 78];
+// console.log(newNumber);
+
+
+// const user = {
+//     name: 'Tusar',
+//     age: 26,
+//     role: 'Web Developer'
+// }
+// console.log(user);
+
+// const newUser = {
+//     ...user,
+//     age: 25,
+//     name: 'Jannat'
+// }
+
+// console.log(newUser);
+
+// const basicInfo = {
+//     name: "Tusar",
+//     age: 28
+// };
+
+// const jobInfo = {
+//     role: "Developer",
+//     company: "Basecom"
+// };
+
+// const info = {
+//     ...basicInfo,
+//     ...jobInfo
+// }
+
+// console.log(info);
+
+
+// const user = {
+//     name: 'Tusar',
+//     age: 26,
+//     role: 'Web Developer'
+// }
+
+// const updateUser = {
+//     ...user,
+//     color: 'Black',
+// }
+
+// console.log(updateUser);
+
+
+// Task 01
+const frontend = ["HTML", "CSS", "JavaScript"];
+const backend = ["Node.js", "Express"];
+const skills = [...frontend, ...backend];
+console.log(skills);
 
 // Task 02
-const user = {
-    name: "Tusar",
-    age: 28
-};
-const {name: userName, age: userAge} = user;
+// const user = {
+//     name: "Tusar",
+//     age: 28,
+//     role: "Developer"
+// };
+
+// const updateUser = {
+//     ...user,
+//     age: 29,
+//     role: 'Senior Developer'
+// }
+// console.log(updateUser);
 
 // Task 03
-const skills = ["HTML", "CSS", "JavaScript", "React"];
-const [firstSkill, secondSkill, thirdSkill, fourthSkill] = skills;
-
-// Task 04
 const user = {
     name: "Tusar",
-    role: "Developer",
-    country: "Bangladesh"
+    age: 28,
+    role: "Developer"
 };
 
-const showUser = user => {
-    const {name, role, country} = user;
-    return `${name} is a ${role} from ${country}`
+const updateAge = (oldUser, newAge) => {
+    const updateUser = {
+        ...oldUser,
+        age: newAge
+    }
+
+    return updateUser;
+} 
+console.log(updateAge(user, 29));
+
+
+// Task 04
+const sum = (...rest) => {
+    let result = 0;
+    rest.forEach(num => result += num)
+    return result;
 }
-showUser(user);
+console.log(sum(10, 20, 30, 40));
